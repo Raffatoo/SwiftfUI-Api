8428370f2c6aaba85e0c89a876bfc7f3adb7755e
Rafael Torres <39065424+Raffatoo@users.noreply.github.com>
2021-06-21 22:03:18 -0500

    New Proyec
    SwiftUI ^ Combine

---
+++ SwiftfUIApi/SwiftfUIApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfUIApiApp.swift
+//  SwiftfUIApi
+//
+//  Created by Cruz Torres on 21/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfUIApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
