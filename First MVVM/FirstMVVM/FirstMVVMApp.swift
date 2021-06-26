64e0cc3402dee050cedeb27cbe944265048d6604
Rafael Torres <39065424+Raffatoo@users.noreply.github.com>
2021-06-26 04:59:44 -0500

    Star Project

---
+++ First MVVM/FirstMVVM/FirstMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstMVVMApp.swift
+//  FirstMVVM
+//
+//  Created by Cruz Torres on 26/06/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
